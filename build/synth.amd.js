d12845c2fe9c0bf8b5203d3c25366265d2c6cdd8
Ben Foxall <ben@pusher.com>
2016-09-08 09:32:11 +0200

    new build, connected

--- build/synth.amd.js
+++ build/synth.amd.js
@@ -1,13 +1,34 @@
-define(['Tone/instrument/Synth', 'Tone/component/Analyser', 'pusher-js'], function (Synth, Analyser, pusherJs) { 'use strict';
+define(['Tone/instrument/Synth', 'Tone/instrument/PolySynth', 'Tone/component/Analyser', 'pusher-js'], function (Synth, PolySynth, Analyser, Pusher) { 'use strict';
 
 Synth = 'default' in Synth ? Synth['default'] : Synth;
+PolySynth = 'default' in PolySynth ? PolySynth['default'] : PolySynth;
 Analyser = 'default' in Analyser ? Analyser['default'] : Analyser;
+Pusher = 'default' in Pusher ? Pusher['default'] : Pusher;
 
-var frequency = function (note) { return 440 * Math.pow(2, (note-69)/17); }
+var noteToFrequency = function (note) { return 440 * Math.pow(2, (note-69)/17); }
 
 var fft = new Analyser('fft', 32)
 var waveform = new Analyser('waveform', 1024)
-var synth = new Synth().fan(fft, waveform).toMaster()
+
+
+// const synth = new Synth().fan(fft, waveform).toMaster()
+
+var synth = new PolySynth(3, Synth, {
+  "oscillator" : {
+  "type" : "fatsawtooth",
+    "count" : 3,
+    "spread" : 30
+  },
+  "envelope": {
+    "attack": 0.01,
+    "decay": 0.1,
+    "sustain": 0.5,
+    "release": 0.4,
+    "attackCurve" : "exponential"
+  },
+})
+.fan(fft, waveform).toMaster();
+
 
 
 var canvas = document.createElement('canvas')
@@ -52,11 +73,25 @@ function draw(){
 requestAnimationFrame(draw)
 
 
-setInterval(function(){
-  var note = 27 + ~~(Math.random() * 50)
+var pusher = new Pusher('dc6a0545f9f460f6c5e1', {cluster: 'eu', encrypted: true})
 
-  synth.triggerAttackRelease(frequency(note), "4n")
+var channel = pusher.subscribe('midi')
+channel.bind('message', function (m) {
+  var f = noteToFrequency(m[1])
 
-}, 560)
+  if(m[0]==144) {
+    synth.triggerAttack(f)
+  }
+  if(m[0]==128) {
+    synth.triggerRelease(f)
+  }
+})
+
+// setInterval(function(){
+//   var note = 27 + ~~(Math.random() * 50)
+//
+//   synth.triggerAttackRelease(frequency(note), "4n")
+//
+// }, 560)
 
 });
